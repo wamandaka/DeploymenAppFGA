bd42a30a9854eaede7c03c58fca22daa0046989a
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-09-12 20:26:37 +0700

    initial

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error("Division by zero");
+  }
+  return a / b;
+}
+
+--------------------------------------------
+pembelajaran RegEXP / REGEX
+
+// check function
+function isNameValid(name) {
+  return /^[a-zA-Z]+$/.test(name);
+}
+
+--------------------------------------------
+
+// check REST Api
+function getAPI(url, callback) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => callback(data))
+    .catch((error) => console.error("Error fetching data: " + error));
+}
